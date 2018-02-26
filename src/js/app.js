55a6908804936d3f6c6ef2927e2d1826c921b68f
Sam Harvey <s.rharveyiv@gmail.com>
2018-02-25 18:50:10 -0700

    slides
    updated to slides

--- src/js/app.js
+++ src/js/app.js
@@ -1,222 +1,116 @@
-
+$(function(){
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
     
-    (function() {
-
-        function buildQuiz() {
-            // we'll need a place to store the HTML output
-            const output = [];
-
-            // for each question...
-            quizFriends.forEach(
-                (currentQuestion, questionNumber) => {
-
-                    // we'll want to store the list of answer choices
-                    const answers = [];
-
-                    // and for each available answer...
-                    for(letter in currentQuestion.answers) {
-
-                        // ...add an HTML radio button
-                        answers.push(
-                            `<label>
-                                <input type="radio" name="question${questionNumber}" value="${letter}">
-                                ${letter} :
-                                ${currentQuestion.answers[letter]} 
-                            </label>`
-                        );
-                    }
-
-                    // add this question and its answers to the output
-                    output.push(
-                        `<div class="question"> ${currentQuestion.question} </div>
-                        <div class="answers"> ${answers.join('')} </div>`
-                    );
-                }
+        // for each question...
+        quizFriends.forEach((currentQuestion, questionNumber) => {
+          // we'll want to store the list of answer choices
+          const answers = [];
+    
+          // and for each available answer...
+          for (letter in currentQuestion.answers) {
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                 <input type="radio" name="question${questionNumber}" value="${letter}">
+                  ${letter} :
+                  ${currentQuestion.answers[letter]}
+               </label>`
             );
-
-            // finally combine out output list into one string of HTML and put it on the page
-            quizContainer.innerHTML = output.join('');
+          }
+    
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+               <div class="question"> ${currentQuestion.question} </div>
+               <div class="answers"> ${answers.join("")} </div>
+             </div>`
+          );
+        });
+    
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+      }
+    
+      function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+    
+        // keep track of user's answers
+        let numCorrect = 0;
+    
+        // for each question...
+        quizFriends.forEach((currentQuestion, questionNumber) => {
+          // find selected answer
+          const answerContainer = answerContainers[questionNumber];
+          const selector = `input[name=question${questionNumber}]:checked`;
+          const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    
+          // if answer is correct
+          if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+    
+            // color the answers green
+            answerContainers[questionNumber].style.color = "lightgreen";
+          } else {
+            // if answer is wrong or blank
+            // color the answers red
+            answerContainers[questionNumber].style.color = "red";
+          }
+        });
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${quizFriends.length}`;
+      }
+    
+      function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+        
+        if (currentSlide === 0) {
+          previousButton.style.display = "none";
+        } else {
+          previousButton.style.display = "inline-block";
         }
-
-        function showResults() {
-            
-            // gather answer containers from out quiz
-            const asnwerContainers = quizContainer.querySelectorAll('.answers');
-
-            // keep track of user's answers
-            let numCorrect = 0;
-
-            // for each question...
-            quizFriends.forEach()( (currentQuestion, questionNumber) => {
-
-                // find selected answer
-                const answswerContainer = asnwerContainers[questionNumber];
-                const selector = 'input[name=question'+questionNumber+']:checked';
-                const userAnswer = (answswerContainer.$(selector) || {}).value;
-
-                // if answer is correct
-                if(userAnswer === currentQuestion.correctAnswer) {
-                    // add to the number of correct answers
-                    numCorrect++;
-
-                    // color the answers green
-                    asnwerContainers[questionNumber].css('color', 'green');
-
-                    // if answer is wrong
-                } else {
-                    // color the answer red
-                    asnwerContainers[questionNumber].css('color', 'red');
-                }
-            });
-
-            // show number of correct answers out of total
-            resultsContainer.innerHTML = `${numbCorrect} out of ${quizFriends.length}`;
+        
+        if (currentSlide === slides.length - 1) {
+          nextButton.style.display = "none";
+          submitButton.style.display = "inline-block";
+        } else {
+          nextButton.style.display = "inline-block";
+          submitButton.style.display = "none";
         }
-
-        // Select HTML tags and store references to these elements in variables
-        const quizContainer = $('#quiz');
-        const resultsContainer = $('#results');
-        const submitButton = $('#submit');
-        const quizFriends = [
-            {
-                question: 'Where were Ross and Emily supposed to go for their honeymoon?',
-                answers: {
-                    a: 'Brazil',
-                    b: 'Germany',
-                    c: 'Athens',
-                    d: 'Hawaii'
-                },
-                correctAnswer: 'c',
-                answerComment: 'In "The One After Ross Says Rachel", Ross is seen waiting for Emily at the airport. The gate agent announces on the P.A. "All ticketed passengers for Flight 1066 to Athens should now be on board", to this Ross says "I get it!"'
-            },
-            {
-                question: 'How many cushions are there on the coffee house sofa',
-                answers: {
-                    a: '1',
-                    b: '3',
-                    c: '4',
-                    d: '5'
-                },
-                correctAnswer: 'a',
-                answerComment: 'Many things have changed in the coffee house over the years, but the couch has stayed the same. The couch must be very comfortable by now.'
-            },
-            {
-                question: "What is the name of Isaac's sister?",
-                answers: {
-                    a: 'Mary',
-                    b: 'Jasmine',
-                    c: 'Jennifer',
-                    d: 'Elizabeth'
-                },
-                correctAnswer: 'b',
-                answerComment: ''
-            },
-            {
-                question: 'What was the name of the bald headed girl who Ross dumps for Rachel at the beach?',
-                answers: {
-                    a: 'Bonnie',
-                    b: 'Rachel',
-                    c: 'Betty',
-                    d: 'Jonnie'
-                },
-                correctAnswer: 'a',
-                answerComment: ''
-            },
-            {
-                question: 'Who directed the episode, "TOW the Ballroom Dancing"?',
-                answers: {
-                    a: 'Gail Mancuso',
-                    b: 'Andrew Tsao',
-                    c: 'Gary Halvorson',
-                    d: 'George Tyne'
-                },
-                correctAnswer: 'a',
-                answerComment: 'This is the fourth episode of the fourth season. Joey hollers at Mr. Treeger for making Rachel cry and because of that, he decides to evict Monica and Rachel. To make things better, Joey agrees to help Mr. Treeger with learning to ballroom dance. Chandler tries to end his membership at a gym but fails.'
-            },
-            {
-                question: "What does Chandler's co-worker, Bob, think Chandler's name is?",
-                answers: {
-                    a: 'Robert',
-                    b: 'Tim',
-                    c: 'Toby',
-                    d: 'Tolbert'
-                },
-                correctAnswer: 'b',
-                answerComment: "After five years, Chandler won't tell Bob the truth. When he learns that Bob is getting promoted up to his office, he sabotages the promotion. When Bob learns that he is not being promoted, he trashes Chandler's office, and even gets Chandler, who Bob still believes is called Toby, to help him."
-            },
-            {
-                question: "What is Joey's PIN number?",
-                answers: {
-                    a: '9365',
-                    b: '6359',
-                    c: '5639',
-                    d: '6545'
-                },
-                correctAnswer: 'c',
-                answerComment: 'They actually spell out Joey.'
-            },
-            {
-                question: "On 'Friends' Joey is on 'Days of Our Lives'. In real life one Friend's father was also on it; which Friend's dad was it?",
-                answers: {
-                    a: 'Courtney Cox',
-                    b: 'Lisa Kudrow',
-                    c: 'Maggie Wheeler',
-                    d: 'Jennifer Aniston'
-                },
-                correctAnswer: 'd',
-                answerComment: "Jennifer's father, John Aniston, has starred for several years on the real daytime drama 'Days of Our Lives'."
-            },
-            {
-                question: 'What is the name of the potential boss that Rachel kissed accidentally in the fifth season?',
-                answers: {
-                    a: 'Mr. Belding',
-                    b: 'Mr. Zelner',
-                    c: 'Mr. Ziegler',
-                    d: 'Mr. Smith'
-                },
-                correctAnswer: 'b',
-                answerComment: "Rachel accidentally kisses him after her first interview, and when he invited her back, she assumes its because of the kiss. Misunderstandings follow and she gets the job despite forgetting Mr. Zelner's name."
-            },
-            {
-                question: 'Joey says that giving Rachel pickles during her pregnancy is like giving him what?',
-                answers: {
-                    a: 'Salad',
-                    b: 'Shake',
-                    c: 'Pop Tart',
-                    d: 'Cookie'
-                },
-                correctAnswer: 'a',
-                answerComment: 'Ross gives Rachel a tuna sandwich with pickles not knowing that pickles make her sick. Ross gets upset that Joey knows more about Rachel than he does.'
-            },
-            {
-                question: 'What does Ross call "Cute Coffee House Guy"?',
-                answers: {
-                    a: 'Burp While He Eats',
-                    b: 'Fart While He Poops',
-                    c: 'Whistles While He Washes Hands',
-                    d: 'Hums While He Pees'
-                },
-                correctAnswer: 'd',
-                answerComment: 'Phoebe ends up dating "Hums While He Pees", and Ross dates his ex-wife.'
-            },
-            {
-                question: "According to Joey's sperm bank file, what rules?",
-                answers: {
-                    a: 'Islanders',
-                    b: 'Knicks',
-                    c: 'Rangers',
-                    d: 'Giants'
-                },
-                correctAnswer: 'b',
-                answerComment: "'The New York Knicks rule' is Joey's personal quote in his file at the sperm bank."
-            },
-        ];
-
-        // display quiz right away
-        buildQuiz();
-
-        // on submit, show results
-        submitButton.on('click', showResults);
+      }
+    
+      function showNextSlide() {
+        showSlide(currentSlide + 1);
+      }
+    
+      function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+      }
+    
+      const quizContainer = document.getElementById("quiz");
+      const resultsContainer = document.getElementById("results");
+      const submitButton = document.getElementById("submit");
+    
+      // display quiz right away
+      buildQuiz();
+    
+      const previousButton = document.getElementById("previous");
+      const nextButton = document.getElementById("next");
+      const slides = document.querySelectorAll(".slide");
+      let currentSlide = 0;
+    
+      showSlide(0);
+    
+      // on submit, show results
+      submitButton.addEventListener("click", showResults);
+      previousButton.addEventListener("click", showPreviousSlide);
+      nextButton.addEventListener("click", showNextSlide);
     });
 
     
--- src/js/friends.js
+++ src/js/friends.js
@@ -1,134 +1,134 @@
-const quizFriends = [
-    {
-        question: 'Where were Ross and Emily supposed to go for their honeymoon?',
-        answers: {
-            a: 'Brazil',
-            b: 'Germany',
-            c: 'Athens',
-            d: 'Hawaii'
+    const quizFriends = [
+        {
+            question: 'Where were Ross and Emily supposed to go for their honeymoon?',
+            answers: {
+                a: 'Brazil',
+                b: 'Germany',
+                c: 'Athens',
+                d: 'Hawaii'
+            },
+            correctAnswer: 'c',
+            answerComment: 'In "The One After Ross Says Rachel", Ross is seen waiting for Emily at the airport. The gate agent announces on the P.A. "All ticketed passengers for Flight 1066 to Athens should now be on board", to this Ross says "I get it!"'
         },
-        correctAnswer: 'c',
-        answerComment: 'In "The One After Ross Says Rachel", Ross is seen waiting for Emily at the airport. The gate agent announces on the P.A. "All ticketed passengers for Flight 1066 to Athens should now be on board", to this Ross says "I get it!"'
-    },
-    {
-        question: 'How many cushions are there on the coffee house sofa',
-        answers: {
-            a: '1',
-            b: '3',
-            c: '4',
-            d: '5'
+        {
+            question: 'How many cushions are there on the coffee house sofa',
+            answers: {
+                a: '1',
+                b: '3',
+                c: '4',
+                d: '5'
+            },
+            correctAnswer: 'a',
+            answerComment: 'Many things have changed in the coffee house over the years, but the couch has stayed the same. The couch must be very comfortable by now.'
         },
-        correctAnswer: 'a',
-        answerComment: 'Many things have changed in the coffee house over the years, but the couch has stayed the same. The couch must be very comfortable by now.'
-    },
-    {
-        question: "What is the name of Isaac's sister?",
-        answers: {
-            a: 'Mary',
-            b: 'Jasmine',
-            c: 'Jennifer',
-            d: 'Elizabeth'
+        {
+            question: "What is the name of Isaac's sister?",
+            answers: {
+                a: 'Mary',
+                b: 'Jasmine',
+                c: 'Jennifer',
+                d: 'Elizabeth'
+            },
+            correctAnswer: 'b',
+            answerComment: ''
         },
-        correctAnswer: 'b',
-        answerComment: ''
-    },
-    {
-        question: 'What was the name of the bald headed girl who Ross dumps for Rachel at the beach?',
-        answers: {
-            a: 'Bonnie',
-            b: 'Rachel',
-            c: 'Betty',
-            d: 'Jonnie'
+        {
+            question: 'What was the name of the bald headed girl who Ross dumps for Rachel at the beach?',
+            answers: {
+                a: 'Bonnie',
+                b: 'Rachel',
+                c: 'Betty',
+                d: 'Jonnie'
+            },
+            correctAnswer: 'a',
+            answerComment: ''
         },
-        correctAnswer: 'a',
-        answerComment: ''
-    },
-    {
-        question: 'Who directed the episode, "TOW the Ballroom Dancing"?',
-        answers: {
-            a: 'Gail Mancuso',
-            b: 'Andrew Tsao',
-            c: 'Gary Halvorson',
-            d: 'George Tyne'
+        {
+            question: 'Who directed the episode, "TOW the Ballroom Dancing"?',
+            answers: {
+                a: 'Gail Mancuso',
+                b: 'Andrew Tsao',
+                c: 'Gary Halvorson',
+                d: 'George Tyne'
+            },
+            correctAnswer: 'a',
+            answerComment: 'This is the fourth episode of the fourth season. Joey hollers at Mr. Treeger for making Rachel cry and because of that, he decides to evict Monica and Rachel. To make things better, Joey agrees to help Mr. Treeger with learning to ballroom dance. Chandler tries to end his membership at a gym but fails.'
         },
-        correctAnswer: 'a',
-        answerComment: 'This is the fourth episode of the fourth season. Joey hollers at Mr. Treeger for making Rachel cry and because of that, he decides to evict Monica and Rachel. To make things better, Joey agrees to help Mr. Treeger with learning to ballroom dance. Chandler tries to end his membership at a gym but fails.'
-    },
-    {
-        question: "What does Chandler's co-worker, Bob, think Chandler's name is?",
-        answers: {
-            a: 'Robert',
-            b: 'Tim',
-            c: 'Toby',
-            d: 'Tolbert'
+        {
+            question: "What does Chandler's co-worker, Bob, think Chandler's name is?",
+            answers: {
+                a: 'Robert',
+                b: 'Tim',
+                c: 'Toby',
+                d: 'Tolbert'
+            },
+            correctAnswer: 'c',
+            answerComment: "After five years, Chandler won't tell Bob the truth. When he learns that Bob is getting promoted up to his office, he sabotages the promotion. When Bob learns that he is not being promoted, he trashes Chandler's office, and even gets Chandler, who Bob still believes is called Toby, to help him."
         },
-        correctAnswer: 'b',
-        answerComment: "After five years, Chandler won't tell Bob the truth. When he learns that Bob is getting promoted up to his office, he sabotages the promotion. When Bob learns that he is not being promoted, he trashes Chandler's office, and even gets Chandler, who Bob still believes is called Toby, to help him."
-    },
-    {
-        question: "What is Joey's PIN number?",
-        answers: {
-            a: '9365',
-            b: '6359',
-            c: '5639',
-            d: '6545'
+        {
+            question: "What is Joey's PIN number?",
+            answers: {
+                a: '9365',
+                b: '6359',
+                c: '5639',
+                d: '6545'
+            },
+            correctAnswer: 'c',
+            answerComment: 'They actually spell out Joey.'
         },
-        correctAnswer: 'c',
-        answerComment: 'They actually spell out Joey.'
-    },
-    {
-        question: "On 'Friends' Joey is on 'Days of Our Lives'. In real life one Friend's father was also on it; which Friend's dad was it?",
-        answers: {
-            a: 'Courtney Cox',
-            b: 'Lisa Kudrow',
-            c: 'Maggie Wheeler',
-            d: 'Jennifer Aniston'
+        {
+            question: "On 'Friends' Joey is on 'Days of Our Lives'. In real life one Friend's father was also on it; which Friend's dad was it?",
+            answers: {
+                a: 'Courtney Cox',
+                b: 'Lisa Kudrow',
+                c: 'Maggie Wheeler',
+                d: 'Jennifer Aniston'
+            },
+            correctAnswer: 'd',
+            answerComment: "Jennifer's father, John Aniston, has starred for several years on the real daytime drama 'Days of Our Lives'."
         },
-        correctAnswer: 'd',
-        answerComment: "Jennifer's father, John Aniston, has starred for several years on the real daytime drama 'Days of Our Lives'."
-    },
-    {
-        question: 'What is the name of the potential boss that Rachel kissed accidentally in the fifth season?',
-        answers: {
-            a: 'Mr. Belding',
-            b: 'Mr. Zelner',
-            c: 'Mr. Ziegler',
-            d: 'Mr. Smith'
+        {
+            question: 'What is the name of the potential boss that Rachel kissed accidentally in the fifth season?',
+            answers: {
+                a: 'Mr. Belding',
+                b: 'Mr. Zelner',
+                c: 'Mr. Ziegler',
+                d: 'Mr. Smith'
+            },
+            correctAnswer: 'b',
+            answerComment: "Rachel accidentally kisses him after her first interview, and when he invited her back, she assumes its because of the kiss. Misunderstandings follow and she gets the job despite forgetting Mr. Zelner's name."
         },
-        correctAnswer: 'b',
-        answerComment: "Rachel accidentally kisses him after her first interview, and when he invited her back, she assumes its because of the kiss. Misunderstandings follow and she gets the job despite forgetting Mr. Zelner's name."
-    },
-    {
-        question: 'Joey says that giving Rachel pickles during her pregnancy is like giving him what?',
-        answers: {
-            a: 'Salad',
-            b: 'Shake',
-            c: 'Pop Tart',
-            d: 'Cookie'
+        {
+            question: 'Joey says that giving Rachel pickles during her pregnancy is like giving him what?',
+            answers: {
+                a: 'Salad',
+                b: 'Shake',
+                c: 'Pop Tart',
+                d: 'Cookie'
+            },
+            correctAnswer: 'a',
+            answerComment: 'Ross gives Rachel a tuna sandwich with pickles not knowing that pickles make her sick. Ross gets upset that Joey knows more about Rachel than he does.'
         },
-        correctAnswer: 'a',
-        answerComment: 'Ross gives Rachel a tuna sandwich with pickles not knowing that pickles make her sick. Ross gets upset that Joey knows more about Rachel than he does.'
-    },
-    {
-        question: 'What does Ross call "Cute Coffee House Guy"?',
-        answers: {
-            a: 'Burp While He Eats',
-            b: 'Fart While He Poops',
-            c: 'Whistles While He Washes Hands',
-            d: 'Hums While He Pees'
+        {
+            question: 'What does Ross call "Cute Coffee House Guy"?',
+            answers: {
+                a: 'Burp While He Eats',
+                b: 'Fart While He Poops',
+                c: 'Whistles While He Washes Hands',
+                d: 'Hums While He Pees'
+            },
+            correctAnswer: 'd',
+            answerComment: 'Phoebe ends up dating "Hums While He Pees", and Ross dates his ex-wife.'
         },
-        correctAnswer: 'd',
-        answerComment: 'Phoebe ends up dating "Hums While He Pees", and Ross dates his ex-wife.'
-    },
-    {
-        question: "According to Joey's sperm bank file, what rules?",
-        answers: {
-            a: 'Islanders',
-            b: 'Knicks',
-            c: 'Rangers',
-            d: 'Giants'
+        {
+            question: "According to Joey's sperm bank file, what rules?",
+            answers: {
+                a: 'Islanders',
+                b: 'Knicks',
+                c: 'Rangers',
+                d: 'Giants'
+            },
+            correctAnswer: 'b',
+            answerComment: "'The New York Knicks rule' is Joey's personal quote in his file at the sperm bank."
         },
-        correctAnswer: 'b',
-        answerComment: "'The New York Knicks rule' is Joey's personal quote in his file at the sperm bank."
-    },
-];
+    ];
